138c1a8a5c5d66c83475a18b8c7260f6e27b5ea0
Alistair Begg <begg.a@etoncollege.org.uk>
2021-11-29 18:45:33 +0000

    added more sort tests and sorting methods as well as linear search test

--- StandardAlgorithmsNew/Sorting.swift
+++ StandardAlgorithmsNew/Sorting.swift
@@ -26,5 +26,85 @@ class Sorting {
         }
     }
     
-    func quickSort(
+    func quickSort(_ array: [Int]) -> [Int] {
+        if array.count <= 1 {
+            return array
+          } else {
+            var finalArray: [Int] = []
+            let pivot = array[0]
+            var leftArray: [Int] = []
+            var rightArray: [Int] = []
+            for i in 1..<array.count {
+              if array[i] < pivot {
+                leftArray.append(array[i])
+              } else {
+                rightArray.append(array[i])
+              }
+            }
+            finalArray.append(contentsOf: quickSort(leftArray))
+            finalArray.append(pivot)
+            finalArray.append(contentsOf: quickSort(rightArray))
+            return finalArray
+          }
+    }
+    
+    func insertionSort(_ array: [Int]) -> [Int] {
+        var sortedArray = array
+        for index in 1..<sortedArray.count {
+            var currentIndex = index
+            while currentIndex > 0 && sortedArray[currentIndex] < sortedArray[currentIndex - 1] {
+                sortedArray.swapAt(currentIndex - 1, currentIndex)
+                currentIndex -= 1
+            }
+        }
+        return sortedArray
+    }
+    
+    func merge(_ left: [Int], _ right: [Int]) -> [Int] {
+        var leftIndex = 0
+        var rightIndex = 0
+        var orderedArray: [Int] = []
+        
+        while leftIndex < left.count && rightIndex < right.count {
+          let leftElement = left[leftIndex]
+          let rightElement = right[rightIndex]
+
+          if leftElement < rightElement {
+            orderedArray.append(leftElement)
+            leftIndex += 1
+          } else if leftElement > rightElement {
+            orderedArray.append(rightElement)
+            rightIndex += 1
+          } else {
+            orderedArray.append(leftElement)
+            leftIndex += 1
+            orderedArray.append(rightElement)
+            rightIndex += 1
+          }
+        }
+        
+        while leftIndex < left.count {
+            orderedArray.append(left[leftIndex])
+            leftIndex += 1
+        }
+        
+        while rightIndex < right.count {
+            orderedArray.append(right[rightIndex])
+            rightIndex += 1
+        }
+        
+        return orderedArray
+    }
+    
+    func mergeSort(_ array: [Int]) -> [Int] {
+        if array.count <= 1 {
+            return array
+        } else {
+            let middleIndex = array.count / 2
+            let leftArray = mergeSort(Array(array[0..<middleIndex]))
+            let rightArray = mergeSort(Array(array[middleIndex..<array.count]))
+            
+            return merge(leftArray, rightArray)
+        }
+    }
 }
--- StandardAlgorithmsTest/SortingTest.swift
+++ StandardAlgorithmsTest/SortingTest.swift
@@ -24,6 +24,16 @@ class SortingTest: XCTestCase {
         }
     }
     
+    func testBubbleSortPerformance() {
+        //arrange
+        let sorting = Sorting()
+        let testArray = [1,3,6,2,2]
+        //act
+        measure {
+            _ = sorting.bubbleSort(testArray)
+        }
+    }
+    
     func testQuickSortWithIntegerArrayReturnsSortedIntegerArray() {
         //arrange
         let sorting = Sorting()
@@ -39,13 +49,78 @@ class SortingTest: XCTestCase {
         }
     }
     
-    func testBubbleSortPerformance() {
+    func testQuickSortPerformance() {
         //arrange
         let sorting = Sorting()
         let testArray = [1,3,6,2,2]
         //act
         measure {
-            _ = sorting.bubbleSort(testArray)
+            _ = sorting.quickSort(testArray)
+        }
+    }
+    
+    func testInsertionSortWithIntegerArrayReturnsSortedIntegerArray() {
+        //arrange
+        let sorting = Sorting()
+        let testCases = [([3,5,1,2,9], [1,2,3,5,9]),
+                         ([1,3,2,4,5], [1,2,3,4,5]),
+                         ([Int()], [Int()]),
+                         ([1,2,3,4,5], [1,2,3,4,5])]
+        //act
+        for testCase in testCases {
+            let actual = sorting.insertionSort(testCase.0)
+            //assert
+            XCTAssertEqual(actual, testCase.1)
+        }
+    }
+    
+    func testInsertionSortPerformance() {
+        //arrange
+        let sorting = Sorting()
+        let testArray = [1,3,6,2,2]
+        //act
+        measure {
+            _ = sorting.insertionSort(testArray)
+        }
+    }
+    
+    func testMergeSortWithIntegerArrayReturnsSortedIntegerArray() {
+        //arrange
+        let sorting = Sorting()
+        let testCases = [([3,5,1,2,9], [1,2,3,5,9]),
+                         ([1,3,2,4,5], [1,2,3,4,5]),
+                         ([Int()], [Int()]),
+                         ([1,2,3,4,5], [1,2,3,4,5])]
+        //act
+        for testCase in testCases {
+            let actual = sorting.mergeSort(testCase.0)
+            //assert
+            XCTAssertEqual(actual, testCase.1)
+        }
+    }
+    
+    func testMergeSortPerformance() {
+        //arrange
+        let sorting = Sorting()
+        let testArray = [1,3,6,2,2]
+        //act
+        measure {
+            _ = sorting.mergeSort(testArray)
+        }
+    }
+    
+    func testLinearSearchWithIntegerArrayFindsNumberToFindAndReturnsTrueOrReturnsFalse() {
+        //arrange
+        let searching = Searching()
+        let testCases = [([3,5,1,2,9], 1, true),
+                         ([1,3,2,4,5], 3, true),
+                         ([Int()], 4, false),
+                         ([1,2,3,4,5], 7, false)]
+        //act
+        for testCase in testCases {
+            let actual = searching.linearSearch(testCase.0, testCase.1)
+            //assert
+            XCTAssertEqual(actual, testCase.2)
         }
     }
 }
